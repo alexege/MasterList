020729134fbeb5f23f5b7211e89887990748acad
Alexander Ege <alexege@gmail.com>
2020-01-18 17:18:46 -0800

    js replace * with bullet

--- Migrations/20200116002258_InitialMigration.Designer.cs
+++ Migrations/20200116002258_InitialMigration.Designer.cs
@@ -9,7 +9,7 @@ using masterList.Models;
 namespace masterList.Migrations
 {
     [DbContext(typeof(MyContext))]
-    [Migration("20200114215944_InitialMigration")]
+    [Migration("20200116002258_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -80,6 +80,34 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+function openWordAccordion(word){
+    
+    var element = document.getElementById(word);
+    console.log("Element:", element);
+    var display = element.style.display;
+    console.log("Panel:", element.nextElementSibling);
+    
+    if(display == "none" || display == ""){
+        console.log("Display is hidden");
+        element.nextElementSibling.style.display = "block";
+        // document.getElementById(word).style.display = "inline-block";
+
+        // var panel = document.getElementById()
+        // this.closest(".accordion").nextElementSibling;
+        
+
+
+        // if (panel.style.display === "block") {
+        // panel.style.display = "none";
+        // } else {
+        // panel.style.display = "block";
+        // }
+    } else {
+
+    }
+    console.log("display:", display);
+}
+
 function editWordTitle(event, element) {
     
     var contents = element.closest("button").querySelector(".contents");
@@ -101,6 +129,10 @@ function editWordDefinition(event, element) {
     var contents = element.closest("button").nextElementSibling.querySelector(".contents");
     var isVisible = element.closest(".accordion").nextElementSibling.querySelector("form").style.display;
     
+    if(!element.closest("button").classList.contains("active")){
+        element.closest("button").classList.addEventListener("active");
+    }
+
     if(isVisible == "none" || isVisible == ""){
         element.closest(".accordion").nextElementSibling.querySelector("form").style.display = "block";
         contents.style.display = "none";
@@ -117,6 +149,10 @@ function editWordExample(event, element) {
     var contents = element.closest("button").nextElementSibling.querySelector(".contents");
     var isVisible = element.closest(".accordion").nextElementSibling.querySelector("form").style.display;
     
+    if(!element.closest("button").classList.contains("active")){
+        element.closest("button").classList.addEventListener("active");
+    }
+
     if(isVisible == "none" || isVisible == ""){
         element.closest(".accordion").nextElementSibling.querySelector("#updateExampleForm").style.display = "block";
         contents.style.display = "none";
@@ -127,3 +163,81 @@ function editWordExample(event, element) {
         isVisible = "none";
     }
 }
+
+// Display word configuration menu
+function showMenu(word){
+    word.querySelector('.menu').style.display = "inline-block";
+}
+
+// Hide word configuration menu
+function hideMenu(word){
+    word.querySelector('.menu').style.display = "none";
+}
+
+// ob = window.document.getElementById("check");
+// function checkWord(e){
+//     console.log(e.keyCode);
+//     if(e.keyCode === 56){
+//         ob.value = "--";
+//         ob.keyCode = 20;
+//     }
+// }
+// ob.addEventListener("keydown",checkWord); 
+
+// function checkForBullets(event){
+
+//     console.log(e.KeyCode);
+
+//     if(e.Key === '*'){
+//         console.log(" we found one");
+//     }
+
+//     // console.log(event.shiftKey);
+//     // console.log(event.KeyCode);
+//     // console.log(event.Key);
+//     // console.log(event.Code);
+
+
+//     // if(event.KeyCode === 56){
+//     //     console.log("They wrote a *");
+//     // }
+//     // if(event.KeyCode === 42){
+//     //     console.log("They wrote a *");
+//     // }
+//     // if(event.KeyCode === 97){
+//     //     console.log("They wrote a *");
+//     // }
+//     // console.log(element.value);
+// }
+
+var content = document.getElementsByClassName("contents");
+console.log(content);
+for(var i = 0; i < content.length; i++){
+    console.log("content:", content[i].innerText);
+    var idx = 0;
+    if(content[i].innerText[idx] == " "){
+        while(content[i].innerText[idx] == " "){
+            idx++;
+        }
+    }
+    if(content[i].innerText[idx] == '*'){
+        console.log("Starts with a *");
+
+        console.log(content[i]);
+
+        content[i].innerHTML = `<li>${content[i].innerText}</li>`;
+        
+        // var newstr = document.createTextNode(content[i].innerText);
+        // newstr.setAttribute()
+        // prepend("<li>");
+        // newstr.append("</li>");
+        // document.body.append(newstr);
+        // content[i].innerText.replace("*", "--");
+        // content.value.replace("*", "--");
+    }
+}
+
+
+var replace = document.getElementById("replace").innerText;
+replace.replace("string", "doggo");
+console.log("replace:", replace);
